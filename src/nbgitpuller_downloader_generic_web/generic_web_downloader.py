fd16fbde5e0c99ef7068fe8341e1bb48d18b1f31
Sean Morris <sean.smorris@berkeley.edu>
2021-11-24 12:52:51 -0800

    Synced to nbgitpuller
    - changed signature for handle_files
    - changed name of import hookspecs to plugin_hook_specs

--- src/nbgitpuller_downloader_generic_web/generic_web_downloader.py
+++ src/nbgitpuller_downloader_generic_web/generic_web_downloader.py
@@ -1,32 +1,17 @@
 from nbgitpuller.plugin_helper import handle_files_helper
-from nbgitpuller.plugin_helper import extract_file_extension
-from nbgitpuller.hookspecs import hookimpl
+from nbgitpuller.plugin_hook_specs import hookimpl
 import asyncio
 
-
-def determine_file_extension(url):
-    """
-    :param str url: url to source
-    :return the extension indicating the file compression(e.g. zip, tgz)
-    :rtype str
-    """
-    return extract_file_extension(url)
-
-
 @hookimpl
-def handle_files(query_line_args):
+def handle_files(helper_args, query_line_args):
     """
-    :param json args: this includes any argument you put on the url
-    PLUS the function, query_line_args["progress_func"], that writes messages to
-    the progress stream in the browser window and the download_q,
-    query_line_args["download_q"] the progress function uses.
+    :param dict helper_args: the function, helper_args["progress_func"], that writes messages to
+    the progress stream in the browser window and the download_q, helper_args["download_q"] the progress function uses.
+    :param dict query_line_args: this includes all the arguments included on the nbgitpuller URL
     :return two parameter json unzip_dir and origin_repo_path
     :rtype json object
     """
-    ext = determine_file_extension(query_line_args["repo"])
-    query_line_args["extension"] = ext
-
     loop = asyncio.get_event_loop()
-    tasks = handle_files_helper(query_line_args), query_line_args["progress_func"]()
+    tasks = handle_files_helper(helper_args, query_line_args), helper_args["wait_for_sync_progress_queue"]()
     result_handle, _ = loop.run_until_complete(asyncio.gather(*tasks))
     return result_handle
